89c0d585a256a30159c44a67dfa0790bc687dcc8
Will Rogers <wjrogers@gmail.com>
2008-01-27 11:03:29 -0500

    Added InputDialog.

---
+++ HeavyDuck.Utilities/Forms/InputDialog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HeavyDuck.Utilities.Forms
+{
+    public partial class InputDialog : Form
+    {
+        private InputDialog()
+        {
+            InitializeComponent();
+        }
+
+        public static DialogResult ShowDialog(IWin32Window parent, string title, string prompt, ref string value)
+        {
+            InputDialog dialog;
+            DialogResult result;
+
+            // initialize the dialog
+            dialog = new InputDialog();
+            dialog.Text = title;
+            dialog.prompt_label.Text = prompt;
+            dialog.input_box.Text = value;
+
+            // show the dialog
+            result = dialog.ShowDialog(parent);
+
+            // set the output and return
+            value = dialog.input_box.Text;
+            return result;
+        }
+    }
+}
