da4a22c896dd9df87b6c7aeca19ab06cec90b886
Will Rogers <WillRogers@CHAUCER>
2008-01-27 11:03:29 -0500

    Added InputDialog.

---
+++ HeavyDuck.Utilities/Forms/InputDialog.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace HeavyDuck.Utilities.Forms
+{
+    partial class InputDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prompt_label = new System.Windows.Forms.Label();
+            this.input_box = new System.Windows.Forms.TextBox();
+            this.ok_button = new System.Windows.Forms.Button();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // prompt_label
+            // 
+            this.prompt_label.AutoSize = true;
+            this.prompt_label.Location = new System.Drawing.Point(12, 9);
+            this.prompt_label.Name = "prompt_label";
+            this.prompt_label.Size = new System.Drawing.Size(35, 13);
+            this.prompt_label.TabIndex = 0;
+            this.prompt_label.Text = "label1";
+            // 
+            // input_box
+            // 
+            this.input_box.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.input_box.Location = new System.Drawing.Point(12, 25);
+            this.input_box.Name = "input_box";
+            this.input_box.Size = new System.Drawing.Size(370, 20);
+            this.input_box.TabIndex = 1;
+            // 
+            // ok_button
+            // 
+            this.ok_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ok_button.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.ok_button.Location = new System.Drawing.Point(226, 66);
+            this.ok_button.Name = "ok_button";
+            this.ok_button.Size = new System.Drawing.Size(75, 23);
+            this.ok_button.TabIndex = 2;
+            this.ok_button.Text = "Ok";
+            this.ok_button.UseVisualStyleBackColor = true;
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancel_button.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancel_button.Location = new System.Drawing.Point(307, 66);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(75, 23);
+            this.cancel_button.TabIndex = 3;
+            this.cancel_button.Text = "Cancel";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            // 
+            // InputDialog
+            // 
+            this.AcceptButton = this.ok_button;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancel_button;
+            this.ClientSize = new System.Drawing.Size(394, 101);
+            this.ControlBox = false;
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.ok_button);
+            this.Controls.Add(this.input_box);
+            this.Controls.Add(this.prompt_label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "InputDialog";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Input";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label prompt_label;
+        private System.Windows.Forms.TextBox input_box;
+        private System.Windows.Forms.Button ok_button;
+        private System.Windows.Forms.Button cancel_button;
+    }
+}
