64b3f886b01b44603df95f7c4d30308159fe82b5
Will Rogers <wjrogers@gmail.com>
2008-02-12 20:51:56 -0500

    Added OptionsDialog. Added Util class for miscellaneous utility methods.

---
+++ HeavyDuck.Utilities/Util.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeavyDuck.Utilities
+{
+    public static class Util
+    {
+        public static TEnum EnumParse<TEnum>(string value)
+        {
+            return (TEnum)Enum.Parse(typeof(TEnum), value);
+        }
+    }
+}
